aadf0e4f156ae5a1f878d25d90511fb0307a1a34
Stephen R. Hamilton <stephen.r.hamilton@gmail.com>
2012-09-08 14:19:55 -0400

    colors mainly

--- js/prebixi.js
+++ js/prebixi.js
@@ -1,11 +1,19 @@
+var convertMinutes = function(minutes) {
+  var hours = minutes % 24;
+  var relative_minutes = minutes / 24;
+  return String(hours) + ":" + String(minutes); 
+}
+
 $(function() {
   console.log("Hello, world");
   $('#horizSlider').slider({
     change: function(event, ui) {
       console.log("Value is now: " + ui.value);
+      $("#time").text(convertMinutes(ui.value));
     }
   }).width(470);
   //$.getJSON('bixidata.json', function(bixiData) {
   //});
   $("#dp").datepicker();
+
 });
